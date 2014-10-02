e0ecbb1efa991c6ddc14e8da13073941b83383ef
sparso <sam@parso.co.uk>
2014-10-02 22:01:58 +0100

    initial logback stuff

---
+++ spring-logback-javaconfig/src/main/java/uk/co/parso/barebones/SpringConfig.java
@@ -0,0 +1,32 @@
+package uk.co.parso.barebones;
+
+import ch.qos.logback.classic.LoggerContext;
+import javax.annotation.PreDestroy;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Root application context configuration
+ * 
+ * @author sam
+ */
+@Configuration
+// Import Spring MVC configuration, which does things like enable 
+// the use of @Controller
+@EnableWebMvc
+// Scan for annotated classes
+@ComponentScan(basePackages = {"uk.co.parso"})
+public class SpringConfig {
+    @PreDestroy
+    public void preDestroy() {
+        // Shutdown Logback cleanly due to an SMTPAppender bug
+        ILoggerFactory factory = LoggerFactory.getILoggerFactory();
+        if (factory instanceof LoggerContext) {
+            LoggerContext ctx = (LoggerContext) factory;
+            ctx.stop();
+        }
+    }
+}
---
+++ spring-logback-javaconfig/src/main/java/uk/co/parso/barebones/TestController.java
@@ -0,0 +1,29 @@
+package uk.co.parso.barebones;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Basic REST controller
+ * 
+ * @author sam
+ */
+@Controller
+@RequestMapping("/")
+public class TestController {
+    
+    private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String ack() {
+        logger.debug("This is some debug");
+        logger.warn("This is a warning");
+        logger.error("This is an error");
+        
+        return "ack";
+    }
+}
