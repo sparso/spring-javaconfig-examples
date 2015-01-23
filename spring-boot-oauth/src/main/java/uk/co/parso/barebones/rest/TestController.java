c2dda702b353d0d5c04fd85431223b7da1ce1063
Sam Parsons <sam@Sams-iMac.local>
2015-01-23 16:22:12 +0000

    started oauth project

---
+++ spring-boot-oauth/src/main/java/uk/co/parso/barebones/rest/TestController.java
@@ -0,0 +1,31 @@
+package uk.co.parso.barebones.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Basic REST controller
+ * 
+ * @author sam
+ */
+@Controller
+@RequestMapping("/")
+public class TestController {
+    
+    private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String ack() {
+        logger.debug("This is some debug");
+        logger.warn("This is a warning");
+        
+        Exception ex = new Exception("test exception");
+        logger.error("This is an error",ex);
+        
+        return "ack";
+    }
+}
